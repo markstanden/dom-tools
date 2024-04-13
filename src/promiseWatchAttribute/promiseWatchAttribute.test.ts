b7bde4cf687f1e9e9d6c01e2be64029f7a5e70d8
Mark Standen <no-reply@rundog.dev>
2024-04-13 08:06:55 +0100

    Updates to promiseWatchAttribute.  - Fixed asynchronous test

--- src/promiseWatchAttribute/promiseWatchAttribute.test.ts
+++ src/promiseWatchAttribute/promiseWatchAttribute.test.ts
@@ -1,23 +1,11 @@
-import {promiseWatchAttribute} from "./watchAttribute.ts";
+import {promiseWatchAttribute} from "./promiseWatchAttribute.ts";
 import {createTestButton} from "../__testHelpers/createTestButton.ts";
 import {expect} from "vitest";
 
 // @vitest-environment happy-dom
-describe.concurrent('initial test', () => {
+describe.concurrent('promiseWatchAttribute tests:', () => {
 
-    test.concurrent('callback should not be called if the attribute does not mutate', async (test) => {
-        const testName = test.task.name;
-        const buttonName = `watchAttribute-${testName}`;
-        const testButton = createTestButton(buttonName);
-        document.body.appendChild(testButton)
-
-        const promise = promiseWatchAttribute(testButton, "data-test");
-
-        expect(testButton.getAttribute("data-test")).toEqual(buttonName);
-        expect(promise).not.resolves;
-    });
-
-    test.concurrent('callback should be called if the attribute mutates', async (test) => {
+    test.concurrent('Promise should resolve to the new value if the attribute mutates', async (test) => {
         const testName = test.task.name;
         const buttonName = `watchAttribute-${testName}`;
         const testButton = createTestButton(buttonName);
@@ -25,9 +13,14 @@ describe.concurrent('initial test', () => {
 
         const promise = promiseWatchAttribute(testButton, "data-test")
 
-        testButton.setAttribute("data-test", "mutated-value")
+        // asynchronously change the attribute value at the next tick of the task queue
+        setTimeout(() => testButton.setAttribute("data-test", "mutated-value"))
 
+        // Synchronous assertion - button attribute has not changed yet.
+        expect(testButton.getAttribute("data-test")).toEqual(buttonName);
+
+        // Asynchronous assertions - wait for the promise to resolve.
+        await expect(promise).resolves.toEqual("mutated-value")
         expect(testButton.getAttribute("data-test")).toEqual("mutated-value");
-        expect(promise).resolves.toEqual("mutated-value")
     });
 })
--- src/promiseWatchAttribute/promiseWatchAttribute.ts
+++ src/promiseWatchAttribute/promiseWatchAttribute.ts
@@ -1,6 +1,6 @@
 /**
  * Function adds a mutation observer to watch for changes to the passed html attribute.
- * If it is changed it will invoke the passed callback.
+ * On attribute change the promise is resolved.
  * @param { HTMLElement } elementWithAttribute The element to watch for the attribute change
  * @param { string } attributeName The attribute to watch for changes
  * @returns { Promise<string> }
@@ -8,22 +8,25 @@
 export function promiseWatchAttribute(
     elementWithAttribute: HTMLElement,
     attributeName: string,
-) {
+): Promise<string> {
     return new Promise((resolve, reject) =>
     {
-        //TODO: Add reject logic.
-        const observer = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                if (mutation.attributeName === attributeName) {
-                    resolve(
-                        elementWithAttribute.getAttribute(attributeName) ?? ''
-                    );
-                }
-            });
-        });
+        if(!elementWithAttribute){
+            reject("Element not found");
+        }
 
+        const observer = new MutationObserver(mutationHandler);
         observer.observe(elementWithAttribute, {
             attributes: true,
         });
+
+        function mutationHandler(mutations: MutationRecord[], observer: MutationObserver): void {
+            mutations.forEach((mutation) => {
+                if (mutation.attributeName === attributeName) {
+                    resolve(elementWithAttribute.getAttribute(attributeName) ?? "");
+                    observer.disconnect();
+                }
+            });
+        }
     })
 }
