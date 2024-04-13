29b0ae4c453295dd2d364f70ded052727188bcbf
Mark Standen <no-reply@rundog.dev>
2024-04-13 15:19:46 +0100

    Updated documentation for promiseAllElements, removed commented code.

--- src/promiseAllElements/promiseAllElements.ts
+++ src/promiseAllElements/promiseAllElements.ts
@@ -3,14 +3,11 @@ import { promiseElement, PromiseElementConfig } from '../promiseElement/promiseE
 /**
  * Returns a promise of an element that may or may not be present in the DOM.
  * @param {string[]} selectors The CSS selector of the element to return when available
- * @param config
+ * @param {PromiseElementConfig?} config
  * @returns {Promise<Element[]>}
  */
 export function promiseAllElements(selectors: string[], config?: PromiseElementConfig): Promise<Element[]> {
-    const promisedElements = selectors.map(selector => promiseElement(selector, config));
+    const promisedElements = selectors
+        .map(selector => promiseElement(selector, config));
     return Promise.all(promisedElements)
-    // return new Promise((resolve, reject) => {
-    //     const promisedElements = selectors.map(selector => promiseElement(selector));
-    //     return Promise.allSettled(promisedElements)
-    // });
 }
