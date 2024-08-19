653d5111354e269dc23b4d3ad1a3751c8066effa
Mark Standen <no-reply@rundog.dev>
2024-08-19 06:55:03 +0100

    Added small optimisation, comments explaining flow

--- src/promiseElement/promiseElement.ts
+++ src/promiseElement/promiseElement.ts
@@ -12,30 +12,36 @@ export type PromiseElementConfig = {
  * after which the promise is rejected. defaults to 10 seconds
  * @returns {Promise<Element>}
  */
-export function promiseElement<T extends Element>(
+export function promiseElement<T extends Element = Element>(
     selector: string,
-    config?: PromiseElementConfig,
+    config?: PromiseElementConfig
 ): Promise<T> {
     const parent: Element | Document = config?.parent ?? document;
     const maxDuration = config?.maxDuration ?? 10000;
 
     return new Promise((resolve, reject) => {
-        const timeout = setTimeout(
-            () => reject(`element with selector ${selector} not found with parent ${parent.nodeName}`),
-            maxDuration);
+        // If already in the DOM immediately resolve
         const element = parent.querySelector(selector) as T | null;
         if (element) {
             resolve(element);
             return;
-        } else {
-            const mutationObserver = new MutationObserver((_mutations) => {
-                const element = parent.querySelector(selector) as T | null;
-                if (element) {
-                    clearTimeout(timeout);
-                    resolve(element);
-                }
-            });
-            mutationObserver.observe(parent, { childList: true, subtree: true });
         }
+
+        // Not yet in the DOM.
+        const rejectMessage = `element with selector ${selector} not found with parent ${parent.nodeName}`;
+        const timeout = setTimeout(() => reject(rejectMessage), maxDuration);
+
+        // Only recheck when parent has new descendants added
+        const mutationObserver = new MutationObserver((_mutations) => {
+            const element = parent.querySelector(selector) as T | null;
+            if (element) {
+                clearTimeout(timeout);
+                resolve(element);
+            }
+        });
+        mutationObserver.observe(parent, {
+            childList: true,
+            subtree: true,
+        });
     });
 }
